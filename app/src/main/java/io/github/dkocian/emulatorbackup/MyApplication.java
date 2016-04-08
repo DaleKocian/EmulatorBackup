e65c96291455ca6bfe93672c44c061b5da38aced
Dale Kocian <dale.kocian@gmail.com>
2016-04-08 05:46:44 -0500

    remove unused code

--- app/src/main/java/io/github/dkocian/emulatorbackup/MyApplication.java
+++ app/src/main/java/io/github/dkocian/emulatorbackup/MyApplication.java
@@ -1,21 +1,14 @@
 package io.github.dkocian.emulatorbackup;
 
 import android.app.Application;
-import android.content.Context;
 
 import com.firebase.client.Firebase;
 
 public class MyApplication extends Application {
-    private static Context context;
 
     @Override
     public void onCreate() {
         super.onCreate();
         Firebase.setAndroidContext(this);
-        context = this;
-    }
-
-    public static Context getMyApplicationContext() {
-        return context;
     }
 }
